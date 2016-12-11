f48da06435a29c16a698faf70b75d4d8a13fcfba
Mitra Mejia <mitra.mejia@gmail.com>
2016-12-11 16:40:19 -0400

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,42 @@
+// =============================================================
+//
+// Name: index
+// -> Description: 
+//
+// Author: mitramejia 
+// Created at: 12/7/16
+//
+// =============================================================
+
+
+class Rule {
+
+    input;
+    message;
+    success;
+
+    constructor() {
+        this.input = $('#input');
+        this.message = $('#message');
+        this.success = false
+    }
+
+    writeMessage(message) {
+        this.message.text(message)
+    }
+
+    setStatus(status) {
+        this.success = status
+    }
+
+    resolve(success, failure) {
+        if (this.success == true) {
+            success();
+        } else {
+            failure();
+        }
+    }
+
+}
+
+
