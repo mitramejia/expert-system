3f6c1de5d11ab4b0dfefdc0814623cce0d36cbd9
Mitra Mejia <mitra.mejia@gmail.com>
2016-12-13 10:56:05 -0400

    Add correct id

--- js/index.js
+++ js/index.js
@@ -34,37 +34,3 @@ $(document).on("ready", function () {
     });
 
 });
-
-
-/*
- class Rule {
-
- input;
- message;
- success;
-
- constructor() {
- this.input = $('#input');
- this.message = $('#message');
- this.success = false
- }
-
- writeMessage(message) {
- this.message.text(message)
- }
-
- setStatus(status) {
- this.success = status
- }
-
- resolve(success, failure) {
- if (this.success == true) {
- success();
- } else {
- failure();
- }
- }
-
- }
-
- */
